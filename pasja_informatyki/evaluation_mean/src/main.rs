d051418cb932317dfc948194861bc61532fbc8ae
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2024-08-11 18:29:55 +0200

    Add evaluation reader to project evluation_mean

--- pasja_informatyki/evaluation_mean/src/main.rs
+++ pasja_informatyki/evaluation_mean/src/main.rs
@@ -2,7 +2,13 @@ use std::io;
 
 fn main() {
     println!("Please enter value:");
-    let evaluations: Vec<u8> = Vec::new();
+
+    let mut evaluations: Vec<u8> = Vec::new();
+
+    let mut evaluation: u8;
+    io::stdin()
+        .read_line(&mut evaluation)
+        .expect("Failed to read line");
 
 
 }
