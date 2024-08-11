bc81a81f2e8aeda62ba190671f5806b2126f36fc
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2024-08-11 18:46:11 +0200

    Add descrived in comments code to document it to evluation_mean project

--- pasja_informatyki/evaluation_mean/src/main.rs
+++ pasja_informatyki/evaluation_mean/src/main.rs
@@ -1,19 +1,23 @@
 use std::io;
 
 fn main() {
+    //start - ask about values
     println!("Please enter value:");
-
+    
+    //define Vectors
     let mut evaluations: Vec<u8> = Vec::new();
 
+    //define reader of the values
     let mut evaluation: u8;
     while evaluations.len() < 7 {
         io::stdin()
             .read_line(&mut evaluation)
             .expect("Failed to read line");
-        
+        //push read values
         evaluations.push(evaluation);
     }
-
+    
+    //show values to test
     println!("You entered evaluations: ");
     
 
