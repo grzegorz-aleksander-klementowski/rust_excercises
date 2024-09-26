f3291e3adf9592f68856c80d9c13e9c63a33c2a6
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2024-09-26 15:46:38 +0200

    Fixed finally (almost) testing - just to fix test_welcome_message. Changes names from evaluations to grates. Implement add_grates function. Correct grama - from messeges to message.

--- pasja_informatyki/evaluation_mean/src/lib.rs
+++ pasja_informatyki/evaluation_mean/src/lib.rs
@@ -6,16 +6,16 @@ fn add_evaluations(set_of_grates: &mut Vec<f32>, grate: f32) {
 }
 
 
-pub enum Messeges {
+pub enum Messages {
     Welcome,
     PrintSetOfGrates(Vec<f32>),
 }
 
-impl fmt::Display for Messeges {
+impl fmt::Display for Messages {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let message = match self {
-            Messeges::Welcome           => "Please enter value:",
-            Messeges::PrintSetOfGrates(grates)  => "placeholder",
+            Messages::Welcome           => "Please enter value:",
+            Messages::PrintSetOfGrates(grates)  => "placeholder",
         };
         write!(f, "{}", message)
     }
@@ -29,13 +29,13 @@ mod tests {
 
     #[test]
     fn test_welcome_message() {
-        assert_eq!(Messeges::Welcome, "Please enter value:");
+        assert_eq!(Messages::Welcome, "Please enter value:");
     }
 
     #[test] 
     fn test_print_evaluation() {
        let grates_message: Vec<f32>= vec![3.0, 4.5, 5.0 , 3.5, 4.0, 2.5];
-       let message = format!("{}", Messeges::PrintSetOfGrates(grates_message));
+       let message = format!("{}", Messages::PrintSetOfGrates(grates_message));
        assert_eq!(message, "test");
     }
 }
