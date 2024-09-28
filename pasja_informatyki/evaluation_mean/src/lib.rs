217ee52ff068360425a5afd2a5ac7d2ea1b365b3
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2024-09-28 22:37:21 +0200

    Added ErrMessages enum. Added  test for add-grades. Changing add_grades function to add criteria for gradeds and return Result<>

--- pasja_informatyki/evaluation_mean/src/lib.rs
+++ pasja_informatyki/evaluation_mean/src/lib.rs
@@ -1,20 +1,36 @@
 use std::fmt;
 
 
-fn add_gradtes(set_of_grades: &mut Vec<f32>, grade: f32) {
-    set_of_grades.push(grade);
+fn add_grades(set_of_grades: &mut Vec<f32>, grade: f32) -> Result<(), String> {
+    if grade > 1.0 && grade < 6.0 { set_of_grades.push(grade); Ok() }
+    else { Err() } 
 }
 
 
 pub enum Messages {
     Welcome,
     PrintSetOfGrades(Vec<f32>),
+    GradeAdded,
+}
+
+pub enum ErrMessages {
+    GradeOutOfRange,
+}
+
+impl fmt::Display for ErrMessages {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        let err_message = match self {
+        GradeOutOfRange => "Grade is out of range. ",
+        };
+        write!(f, "{}", err_message)
+    }
 }
 
 impl fmt::Display for Messages {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         let message = match self {
             Messages::Welcome                   => "Please enter value: ",
+            Messages::GradeAdded                => "Grade added. ",
             Messages::PrintSetOfGrades(grades)  => "placeholder",
         };
         write!(f, "{}", message)
@@ -39,4 +55,17 @@ mod tests {
        let message = format!("{}", Messages::PrintSetOfGrades(grades_message));
        assert_eq!(message, "Grades:\n3.0, \n4.5, \n5.0, \n3.5, \n4.0, \n2.5");
     }
+
+    #[test]
+    fn test_add_gradetes() {
+        let mut grades: Vec<f32> = Vec::new();
+        let grade_array: [f32; 3] = [5.5, 2.5, 4.0];
+
+        for &grade in grade_array.iter() {
+           add_grades(&mut grades, grade); 
+        }
+    
+        assert_eq!(grades, vec!(5.5, 2.5, 4.0));
+    }
+
 }
