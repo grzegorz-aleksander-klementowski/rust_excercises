be4beee91af64a8f91a433376fcfb4b9557b38cc
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2024-12-04 04:17:44 +0100

    advent of code 2024 day 3 part 1 solve

--- advent_of_code_2024/day_3_mull_it_over/src/main.rs
+++ advent_of_code_2024/day_3_mull_it_over/src/main.rs
@@ -1,25 +1,53 @@
-use std::fs::File;
-use std::io::{self, BufRead };
+use std::fs;
 
 fn main() {
-    let ścieżka_do_pomieszanej_pamięci: &str = "./zapiski/pomieszana_pamięć";
-    let pomieszana_pamięć: File = File::open(ścieżka_do_pomieszanej_pamięci).expect("Nie można odnaleźć pomiedzanej pamięci. Powinno znajdować sie w ./zapiski/pomieszana_pamięć");
-    let czytnik_pomieszanej_pamięci: io::BufReader<File> = io::BufReader::new(pomieszana_pamięć);
+    // Wczytaj zawartość pliku z pomieszaną pamięcią
+    let input = fs::read_to_string("./zapiski/pomieszana_pamięć")
+        .expect("Nie udało się odczytać pliku pomieszana_pamięć");
 
-    // wzorzec: „mut(X,Y)”
-    let początkowy_wzorzec: &str = "mut(";
-    let końcowy_znak: char = ')';
+    let mut total = 0;
+    let input_bytes = input.as_bytes();
+    let input_len = input_bytes.len();
+    let mut i = 0;
 
-    for (liczba_porządkowa, wiersz) in czytnik_pomieszanej_pamięci.lines().enumerate() {
-        let wiersz: String = wiersz.expect("Nie udało się wczytać wiersza");
-        println!("Linia: {}", liczba_porządkowa + 1);
-
-        if let Some(wskaźnik_zaczynający_początkowy_wzorzec) = wiersz.find(początkowy_wzorzec) {
-            let wskaźnik_końcowy_początkowego_wzorca: usize = wskaźnik_zaczynający_początkowy_wzorzec + początkowy_wzorzec.len();
-            //for znak in wiersz[wskaźnik_zaczynający_początkowy_wzorzec..wskaźnik_zaczynający_początkowy_wzorzec+4].chars() {
-                //if znak.is_digit
+    while i < input_len {
+        // Szukamy 'm' jako potencjalny początek 'mul('
+        if input_bytes[i] == b'm' {
+            if i + 3 < input_len && &input_bytes[i..i+4] == b"mul(" {
+                let mut j = i + 4;
+                let mut num1 = String::new();
+                // Parsujemy pierwszą liczbę
+                while j < input_len && input_bytes[j].is_ascii_digit() {
+                    num1.push(input_bytes[j] as char);
+                    j += 1;
+                }
+                // Sprawdzamy czy po pierwszej liczbie jest przecinek
+                if !num1.is_empty() && j < input_len && input_bytes[j] == b',' {
+                    j += 1;
+                    let mut num2 = String::new();
+                    // Parsujemy drugą liczbę
+                    while j < input_len && input_bytes[j].is_ascii_digit() {
+                        num2.push(input_bytes[j] as char);
+                        j += 1;
+                    }
+                    // Sprawdzamy czy po drugiej liczbie jest zamykający nawias
+                    if !num2.is_empty() && j < input_len && input_bytes[j] == b')' {
+                        // Upewniamy się, że liczby mają 1-3 cyfry
+                        if num1.len() <= 3 && num2.len() <= 3 {
+                            let x: i32 = num1.parse().unwrap();
+                            let y: i32 = num2.parse().unwrap();
+                            total += x * y;
+                        }
+                        j += 1;
+                        i = j;
+                        continue;
+                    }
+                }
             }
         }
+        i += 1;
     }
 
+    println!("Suma wyników mnożeń: {}", total);
 }
+
