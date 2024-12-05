ea1e70bf20aaec8e1312631e3c04d08919739363
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2024-12-06 00:54:08 +0100

    advent of code day 5 v2 part 1

--- advent_of_code_2024/day_5_print_queue/src/lib.rs
+++ advent_of_code_2024/day_5_print_queue/src/lib.rs
@@ -3,30 +3,24 @@ use std::fs::File;
 
 const ŚCIEŻKA_DO_POLECEŃ_DRUKARKI: &str = "./input";
 
-trait Czytnik<T> {
-    fn czytnik_zapisku(&self) -> io::BufReader<File>;
-}
 
 struct polecenia_do_drukarki {
     ścieżka: &'static str,
     czytnik_poleceń: io::BufReader<File>,
 }
 
-impl Czytnik<File> for polecenia_do_drukarki {
-    fn czytnik_zapisku(&self) -> io::BufReader<File> {
-        let zapisek: File = File::open(self.ścieżka).expect("Nie można odnaleźć poleceń. Nazwa zapisku winna nosić nazwę „input”");
-        let czytnik_zapisku: io::BufReader<File> = io::BufReader::new(zapisek);
-        return czytnik_zapisku;
-    }
-
-
-}
 impl polecenia_do_drukarki {
     fn nowy ( ścieżka: &'static str ) -> Self {
-        let czytnik_poleceń = Czytnik::czytnik_zapisku(ścieżka);
+        let polecenia: File = File::open(ścieżka).expect("Nie można odnaleźć poleceń. Nazwa zapisku winna nosić nazwę „input”");
+        let czytnik_poleceń: io::BufReader<File> = io::BufReader::new(polecenia);
+
         Self {
             ścieżka,
             czytnik_poleceń
         }
+    }
+
+    fn ilość_zestawień_stron(&self) {
+
     }
 }
