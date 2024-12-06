a3f71fa21cd97cb14bd5d0ef693b778b415c2917
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2024-12-06 11:36:34 +0100

    advent of code day 6 part 1 - start and writing necessary data and variables

---
+++ advent_of_code_2024/day_6_guard_gallivant/src/lib.rs
@@ -0,0 +1,17 @@
+struct Zapisek_mapa_labolatorium {
+    źródło: &'czas_zapisku str,
+}
+
+enum Twór_na_mapie {
+    strażnik,
+    przeszkoda,
+}
+
+struct mapa_labolatorium {
+    szerokość: usize,
+    wysokość: usize,
+}
+
+fn witaj_świecie() {
+
+}
