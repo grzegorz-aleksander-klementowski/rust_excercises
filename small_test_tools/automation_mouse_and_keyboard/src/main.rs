eb4085e17a17c329123ce16e92fc4938b3c6aa2d
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-03-25 21:24:14 +0100

    rust_exercise → small_tools → automation_a_mouse_and_a_keyboard

--- small_test_tools/automation_mouse_and_keyboard/src/main.rs
+++ small_test_tools/automation_mouse_and_keyboard/src/main.rs
@@ -1,3 +1,15 @@
+enum DirectionMouse {
+    Up,
+    Right,
+    Down,
+    Left,
+}
+
+enum InputDevice {
+    Mouse,
+    Keyboard,
+}
+
 fn main() {
     println!("Program for automate a mouse and a keyboard.");
 }
