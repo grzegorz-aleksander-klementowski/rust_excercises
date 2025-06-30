9577b87bef18657be75a3025ea2f521c26473cba
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-07-01 01:04:04 +0200

    small tools → obliczanie_kosztów_cz_rowerowych: The whole program works well!

--- small_test_tools/oblicz-koszty-roweru/src/main.rs
+++ small_test_tools/oblicz-koszty-roweru/src/main.rs
@@ -5,6 +5,8 @@ fn main() {
     let poprzedni_zakup = KosztCzęściRowerowych::new(1190, 1198, 129, 125, 119);
     let nowy_zakup = KosztCzęściRowerowych::new(1190, 1390, 199, 0, 219);
     let różnica = KosztCzęściRowerowych::sub(nowy_zakup, poprzedni_zakup);
+    let wartość_dopłaty = KosztCzęściRowerowych::suma(&różnica);
 
     println!("Różnica w częściach: {}", różnica);
+    println!("Więc nalezy dopłacić: {}", wartość_dopłaty);
 }
