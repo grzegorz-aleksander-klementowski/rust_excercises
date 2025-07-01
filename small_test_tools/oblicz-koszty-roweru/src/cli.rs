5bee1e3e16e8085d297377e865b371bdb1d987fc
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-07-01 18:02:16 +0200

    small tools → obliczanie_kosztów_cz_rowerowych: Added pub to Cli struct. Added values from cli to struct variables in main fn.

--- small_test_tools/oblicz-koszty-roweru/src/cli.rs
+++ small_test_tools/oblicz-koszty-roweru/src/cli.rs
@@ -9,25 +9,27 @@ use clap::Parser;
     about = "The app calculate the differnecets between the costs of the sets of ebike conversion. "
 )]
 pub struct Cli {
+    //nowy zakup (zazwyczaj się wpisuje)
     #[arg(short, long, default_value_t = 1190)]
-    koło_z_silnikiem_nowy: i32,
+    pub koło_z_silnikiem_nowy: i32,
     #[arg(short, long, default_value_t = 1390)]
-    bateria_nowa: i32,
+    pub bateria_nowa: i32,
     #[arg(short, long, default_value_t = 199)]
-    ładowarka_nowa: i32,
+    pub ładowarka_nowa: i32,
     #[arg(short, long, default_value_t = 0)]
-    podstawa_do_baterii_nowa: i32,
+    pub podstawa_do_baterii_nowa: i32,
     #[arg(short, long, default_value_t = 219)]
-    adapter_nowa: i32,
+    pub adapter_nowa: i32,
 
+    // porzedni zakup (zazwyczaj się nie wpisuje)
     #[arg(short, long, default_value_t = 1190)]
-    koło_z_silnikiem_poprzedni: i32,
+    pub koło_z_silnikiem_poprzedni: i32,
     #[arg(short, long, default_value_t = 1198)]
-    bateria_poprzedni: i32,
+    pub bateria_poprzedni: i32,
     #[arg(short, long, default_value_t = 129)]
-    ładowarka_poprzedni: i32,
+    pub ładowarka_poprzedni: i32,
     #[arg(short, long, default_value_t = 125)]
-    podstawa_do_baterii_poprzedni: i32,
+    pub podstawa_do_baterii_poprzedni: i32,
     #[arg(short, long, default_value_t = 119)]
-    adapter_poprzedni: i32,
+    pub adapter_poprzedni: i32,
 }
--- small_test_tools/oblicz-koszty-roweru/src/main.rs
+++ small_test_tools/oblicz-koszty-roweru/src/main.rs
@@ -1,15 +1,37 @@
+pub mod cli;
+use clap::Parser;
+
+use cli::Cli;
 use oblicz_koszty_roweru::{self, KosztCzęściRowerowych};
 use std::{fmt, ops::Sub, process::Output};
 
 fn main() {
-    let poprzedni_zakup = KosztCzęściRowerowych::new(1190, 1198, 129, 125, 119);
-    let nowy_zakup = KosztCzęściRowerowych::new(1190, 1390, 199, 0, 219);
+    // let poprzedni_zakup = KosztCzęściRowerowych::new(1190, 1198, 129, 125, 119);
+    // let nowy_zakup = KosztCzęściRowerowych::new(1190, 1390, 199, 0, 219);
+
+    let cli = Cli::parse();
+
+    let poprzedni_zakup = KosztCzęściRowerowych::new(
+        cli.koło_z_silnikiem_poprzedni,
+        cli.bateria_poprzedni,
+        cli.ładowarka_poprzedni,
+        cli.podstawa_do_baterii_poprzedni,
+        cli.adapter_poprzedni,
+    );
+
+    let nowy_zakup = KosztCzęściRowerowych::new(
+        cli.koło_z_silnikiem_nowy,
+        cli.bateria_nowa,
+        cli.ładowarka_nowa,
+        cli.podstawa_do_baterii_nowa,
+        cli.adapter_nowa,
+    );
 
     println!("Zakupiono pierwszy raz za: {}", &poprzedni_zakup);
     println!("Nowe części kosztują: {}", &nowy_zakup);
 
     let różnica = KosztCzęściRowerowych::sub(nowy_zakup, poprzedni_zakup);
-    let wartość_dopłaty: i32 = KosztCzęściRowerowych::suma(&różnica);
+    let wartość_dopłaty = KosztCzęściRowerowych::suma(&różnica);
 
     println!("Różnica w częściach: {}", różnica);
     println!("Więc nalezy dopłacić: {}", wartość_dopłaty);
