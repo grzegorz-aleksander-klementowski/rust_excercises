057ffc28b18623142d3cd1f05c3513c26329f5a5
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-10-19 21:11:21 +0200

    chore: other/light-state-domain-driven – Added enumeration for LightErrors and wrote structure for the Validation implementation for Light. Now analizing the lifetime issue of `Ok` associated type. The type going to be associated with `Self` (`Light`) type.

--- other/light-state-domain-driven/src/validate.rs
+++ other/light-state-domain-driven/src/validate.rs
@@ -1,10 +1,26 @@
 use std::error::Error;
 
-use crate::Light;
+use crate::{Light, State};
 
 trait Validate {
-    type Ok;
+    type Ok<'a>;
     type Err;
 
-    fn validate_repeatation(&self) -> Result<Self::Ok, Self::Err>;
+    fn validate_repeatation(&self, action: State) -> Result<Self::Ok, Self::Err>;
+}
+
+impl Validate for Light {
+    type Ok<'a> = &'a Self;
+    type Err = LightError;
+
+    fn validate_repeatation(&self, action: State) -> Result<Self::Ok, Self::Err> {
+        if self.is_on() && action == State::Off {
+            todo!()
+        }
+    }
+}
+
+enum LightError {
+    AlredyOn,
+    AlredyOff,
 }
