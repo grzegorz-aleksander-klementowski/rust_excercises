4100a773b2580022c99b67959ea2ac0cab270871
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-10-23 10:12:38 +0200

    fix: other/light-state-domain-driven – fix the problem with lifetimes in `Validation.rs` – it wasn't clear how to use AT (Associated types). The result – use GAT (Generic Associated Types) with bounds (`where`) to ensure that `Ok` will not live more that `Self.

--- other/light-state-domain-driven/src/validate.rs
+++ other/light-state-domain-driven/src/validate.rs
@@ -1,26 +1,32 @@
-use std::error::Error;
-
 use crate::{Light, State};
 
 trait Validate {
-    type Ok<'a>;
+    type Ok<'a_type>
+    where
+        Self: 'a_type;
     type Err;
 
-    fn validate_repeatation(&self, action: State) -> Result<Self::Ok, Self::Err>;
+    fn validate_repeatation<'a_fn>(
+        &'a_fn self,
+        action: State,
+    ) -> Result<Self::Ok<'a_fn>, Self::Err>; // in Self::Ok<'a_fn lifetimes 'a_fn will subsitute 'a_type (subsitution of lifetimes parameters)
 }
 
 impl Validate for Light {
-    type Ok<'a> = &'a Self;
+    type Ok<'a_type> = Self;
     type Err = LightError;
 
-    fn validate_repeatation(&self, action: State) -> Result<Self::Ok, Self::Err> {
+    fn validate_repeatation<'a_fn>(
+        &'a_fn self,
+        action: State,
+    ) -> Result<Self::Ok<'a_fn>, Self::Err> {
         if self.is_on() && action == State::Off {
             todo!()
         }
     }
 }
 
-enum LightError {
+pub enum LightError {
     AlredyOn,
     AlredyOff,
 }
