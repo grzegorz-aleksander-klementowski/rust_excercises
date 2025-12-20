40fda824bee2170e91927ffb5ce00c0bac5af588
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-12-20 16:20:06 +0100

    feat: Added a simpler example of „evaluation_mean” named „simple-example-srednia” of `odc-5` examples

---
+++ pasja_informatyki/odc-5/simple-example-srednia/src/main.rs
@@ -0,0 +1,25 @@
+use std::io::Write;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    const ILOŚĆ_OCEN: usize = 5;
+    let mut oceny = [0.0; ILOŚĆ_OCEN];
+
+    for (wskaźnik, ocena) in oceny.iter_mut().enumerate() {
+        let l_oceny = wskaźnik + 1;
+        print!("Wpisz {l_oceny} ocenę: ");
+        std::io::stdout().flush()?; // uwalnia bufor
+
+        let mut wejście = String::new();
+        std::io::stdin().read_line(&mut wejście)?;
+
+        *ocena = wejście.trim().parse()?;
+    }
+
+    let suma = oceny.iter().sum::<f32>();
+    let ilość_ocen = oceny.iter().len() as f32;
+    let średnia_arytmetyczna = suma / ilość_ocen;
+
+    println!("oceny: {oceny:?}");
+    println!("Średnia arytmetyczna: {średnia_arytmetyczna:.2}");
+    Ok(())
+}
