ccfb09e3ee333437af031b6e52dd308a3a92dbda
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-12-22 10:36:12 +0100

    feat: Written gathering datat and asks to the user to get the data.

--- pasja_informatyki/odc-7/contact-file-managing/src/main.rs
+++ pasja_informatyki/odc-7/contact-file-managing/src/main.rs
@@ -1,3 +1,28 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs::File;
+use std::io::{self, Write};
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // gathering data:
+    // ask about name
+    print!("Podaj imię: ");
+    io::stdout().flush()?;
+    let mut imię = String::new();
+    io::stdin().read_line(&mut imię);
+
+    // ask about lastname
+    print!("Podaj nazwisko: ");
+    io::stdout().flush()?;
+    let mut nazwisko = String::new();
+    io::stdin().read_line(&mut nazwisko);
+
+    // ask about phone number
+    print!("Podaj liczby dalnomówika: ");
+    io::stdout().flush()?;
+    let mut dalnomówink = String::new();
+    io::stdin().read_line(&mut dalnomówink);
+
+    let plik = File::create("wizytówka.txt")?;
+    plik.write_all(b[imię]);
+
+    Ok(())
 }
