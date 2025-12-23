904a57b44b6581a9c4b49f68be8a26271d864ae4
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2025-12-23 23:52:34 +0100

    feat: prepared arrays to keep data from the quiz file according to the tutorial.

--- pasja_informatyki/odc-7/short-quiz/src/main.rs
+++ pasja_informatyki/odc-7/short-quiz/src/main.rs
@@ -1,3 +1,10 @@
 fn main() {
-    println!("Hello, world!");
+    let temat = String::new();
+    let nick = String::new();
+    let treść: [String; 5] = std::array::from_fn(|_| String::new());
+
+    let odpA: [String; 5] = std::array::from_fn(|_| String::new());
+    let odpA: [String; 5] = std::array::from_fn(|_| String::new());
+    let odpA: [String; 5] = std::array::from_fn(|_| String::new());
+    let odpA: [String; 5] = std::array::from_fn(|_| String::new());
 }
