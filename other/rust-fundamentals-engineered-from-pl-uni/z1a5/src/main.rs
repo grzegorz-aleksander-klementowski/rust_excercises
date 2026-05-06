840ce4d14fe742b7f8401beb9e1a48ee74d73db6
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-05-06 23:28:43 +0200

    feat: prepared crate for z1a5 (ex1a5) – time calculating

---
+++ other/rust-fundamentals-engineered-from-pl-uni/z1a5/src/main.rs
@@ -0,0 +1,5 @@
+// 5. Napisz program, który dla danych dwóch poprawnych pór jednej doby (w postaci całkowitych godzin, minut i sekund) wyświetla różnicę czasów (także w postaci analogicznej trójki, z minutami i sekundami w przedziale [0;59]).
+
+fn main() {
+    println!("Hello, world!");
+}
