cc3678c0e62908c9fa300b40aa335acf3e9dd80b
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-05-07 11:47:08 +0200

    feat/other/ex-from-pl-uni/1a5: prepared part of the test function. ToDo: finish it.

--- other/rust-fundamentals-engineered-from-pl-uni/z1a5/src/lib.rs
+++ other/rust-fundamentals-engineered-from-pl-uni/z1a5/src/lib.rs
@@ -1,35 +1,39 @@
 // 5. Napisz program, który dla danych dwóch poprawnych pór jednej doby (w postaci całkowitych godzin, minut i sekund) wyświetla różnicę czasów (także w postaci analogicznej trójki, z minutami i sekundami w przedziale [0;59]).
+// EN: 5. Write a program that, for two valid times of one day (in the form of total hours, minutes and seconds), displays the time difference (also in the form of an analogous triplet, with minutes and seconds in the interval [0;59]).
+#[derive(std::fmt::Debug, PartialEq, Eq)]
+struct Hour(isize);
+#[derive(std::fmt::Debug, PartialEq, Eq)]
+struct Minute(isize);
+#[derive(std::fmt::Debug, PartialEq, Eq)]
+struct Second(isize);
+#[derive(std::fmt::Debug, PartialEq, Eq)]
+struct Time(Hour, Minute, Second);
 
-struct Godzina(isize);
-struct Minuta(isize);
-struct Sekunda(isize);
-struct Czas(Godzina, Minuta, Sekunda);
-
-impl From<Godzina> for Sekunda {
-    fn from(value: Godzina) -> Self {
-        Sekunda(value.0 * 3600)
+impl From<Hour> for Second {
+    fn from(value: Hour) -> Self {
+        Second(value.0 * 3600)
     }
 }
 
-impl From<Sekunda> for Godzina {
-    fn from(value: Sekunda) -> Self {
-        Godzina(value.0 / 3600)
+/* impl From<Second> for Hour {
+    fn from(value: Second) -> Self {
+        Hour(value.0 / 3600)
+    }
+} */
+
+impl From<Minute> for Second {
+    fn from(value: Minute) -> Self {
+        Second(value.0 * 60)
     }
 }
 
-impl From<Minuta> for Sekunda {
-    fn from(value: Minuta) -> Self {
-        Sekunda(value.0 * 60)
+/* impl From<Second> for Minute {
+    fn from(value: Second) -> Self {
+        Minute(value.0 / 60)
     }
-}
+} */
 
-impl From<Sekunda> for Minuta {
-    fn from(value: Sekunda) -> Self {
-        Minuta(value.0 / 60)
-    }
-}
-
-impl std::ops::Sub for Czas {
+impl std::ops::Sub for Time {
     type Output = Self;
     fn sub(self, rhs: Self) -> Self::Output {
         todo!()
@@ -38,7 +42,33 @@ impl std::ops::Sub for Czas {
 
 #[cfg(test)]
 mod test {
-    fn test_różnica_czasu() {
-        todo!()
+    use super::*;
+
+    fn test_difference_time() {
+        let time1 = Time(Hour(2), Minute(25), Second(15));
+        let time2 = Time(Hour(1), Minute(5), Second(45));
+
+        let result = time1 - time2;
+
+        assert_eq!(result, Time(Hour(1), Minute(19), Second(30)));
+    }
+
+    fn test_if_eq_zero() {
+        let time1 = Time(Hour(1), Minute(0), Second(0));
+        let time2 = Time(Hour(1), Minute(0), Second(0));
+
+        let result = time1 - time2;
+
+        assert_eq!(result, Time(Hour(1), Minute(0), Second(0)));
+    }
+
+    fn test_boudaries_ranges() {
+        // TO DO!
+        let time1 = Time(Hour(1), Minute(0), Second(0));
+        let time2 = Time(Hour(1), Minute(0), Second(0));
+
+        let result = time1 - time2;
+
+        assert_eq!(result, Time(Hour(1), Minute(0), Second(0)));
     }
 }
