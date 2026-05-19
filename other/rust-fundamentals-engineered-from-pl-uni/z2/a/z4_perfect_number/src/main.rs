4d5142d2e0a5f5c930a9da48186a57a8cc7e1183
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-05-19 23:44:58 +0200

    feat/other/ex-from-pl-uni/z2a3: Added simple algorithm is separate functin that calculate the Perfect Number problem. Used trait written in another example for input user. Used iterators of the numbers `filter()` and `sum() methods to calculate the problem and get clean matematical view in the function. The problem solved correctly.

---
+++ other/rust-fundamentals-engineered-from-pl-uni/z2/a/z4_perfect_number/src/main.rs
@@ -0,0 +1,24 @@
+// 4. Napisz funkcję, która odpowiada na pytanie, czy jej argument jest liczbą doskonałą.
+//
+// 4. Write a function that answers the question whether its argument is a perfect number.
+
+use gak_core::ask_for_data::AskForData;
+
+fn is_perfect_num(n: usize) -> bool {
+    if n == 0 {
+        return false;
+    }
+
+    let divisors_sum: usize = (1..n).filter(|x| n % x == 0).sum();
+
+    divisors_sum == n
+}
+
+fn main() {
+    let n: usize = AskForData::ask_for_data("Check a number if it's the Perfect Number: ");
+    if is_perfect_num(n) {
+        println!("The number is the Perfect Number! ");
+    } else {
+        println!("The number is NOT the Perfect Number! ");
+    }
+}
