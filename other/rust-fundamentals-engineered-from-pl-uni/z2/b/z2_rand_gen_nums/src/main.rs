695813c2ea5d67cbcb5fd636471a58e1d34866fc
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-05-22 22:52:40 +0200

    fix/other/rust-fundamentals-engineered-from-pl-uni/b2: The exercises in program. Done in a wrong way. Fixing…

--- other/rust-fundamentals-engineered-from-pl-uni/z2/b/z2_rand_gen_nums/src/main.rs
+++ other/rust-fundamentals-engineered-from-pl-uni/z2/b/z2_rand_gen_nums/src/main.rs
@@ -18,12 +18,52 @@
 use askusr::AskForData;
 
 fn rand(seed: &mut usize, min_rand: usize, max_rand: usize) -> usize {
-    todo!()
+    // seed0 → start point
+    // modulo → max_rand
+    // end untill it repeats
+    // bottom row → min_rand
+    // multiplier I chose by myself
+    // min_rand → increment
+
+    let multiplier: usize = 2;
+    let modulus = 9;
+    let increment = 0;
+
+    // `usize` don't allow num < 0
+    if (0 == multiplier) && (multiplier > modulus) && (increment > modulus) {
+        panic!("Incorrect internal value");
+    }
+
+    let lcg = (multiplier * *seed + increment) % modulus;
+    if lcg > max_rand {
+        lcg % (max_rand - min_rand + 1) + min_rand
+    } else {
+        lcg
+    }
 }
 
 fn main() {
-    let seed: usize = AskForData::ask_for_data("Provide the seed to generate a number: ");
-    let min_rand: usize = AskForData::ask_for_data("Provide the seed to generate a number: ");
-    let max_rand: usize = AskForData::ask_for_data("Provide the seed to generate a number: ");
-    println!("to–do: {seed}");
+    let mut seed: usize = AskForData::ask_for_data("Provide the seed to generate a number: ");
+    let min_rand: usize =
+        AskForData::ask_for_data("Provide the minimal number to generate a number: ");
+    let max_rand: usize =
+        AskForData::ask_for_data("Provide the maximal number to generate a number: ");
+
+    let mut lcg_len = 0;
+
+    let lcg0 = rand(&mut seed, min_rand, max_rand);
+    let mut lcg1 = lcg0;
+    let mut lcg2 = 0;
+
+    println!("Random numbers: ");
+    while lcg0 != lcg2 {
+        lcg2 = rand(&mut lcg1, min_rand, max_rand);
+        print!(" {lcg2} ");
+        lcg_len += 1;
+    }
+
+    // (I know the lenght of the LCG is out of task, but I did it.)
+    println!(
+        "The lenght of the LCG (linear congruential generator) with seed {seed}, min num {min_rand}, max num {max_rand} is {lcg_len}"
+    );
 }
