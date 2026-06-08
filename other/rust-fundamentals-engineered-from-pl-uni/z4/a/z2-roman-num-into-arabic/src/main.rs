fb9fcbcd168cefc6248c8cc2f37b8286fd6380e3
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-06-08 17:56:19 +0200

    exercises-uni/z4/2a/test: written signature and tests (TDD approach).

--- other/rust-fundamentals-engineered-from-pl-uni/z4/a/z2-roman-num-into-arabic/src/main.rs
+++ other/rust-fundamentals-engineered-from-pl-uni/z4/a/z2-roman-num-into-arabic/src/main.rs
@@ -16,20 +16,72 @@
 2. Write a function about the header
 
    ```
-   Roman fn(inscription: ...) -> ...
+   fn roman(inscription: ...) -> ...
    ```
 
    which, for a string representing a number in Roman writing (we assume its correctness), returns the number represented by the string. Examples:
 
    ```
-   Roman(III) == 3
-   Roman(IX) == 9
-   Roman(XIX) == 19
-   Roman(MCMX) == 1910
+   Roman("III") == 3
+   Roman("IX") == 9
+   Roman("XIX") == 19
+   Roman("MCMX") == 1910
    ```
 
 */
 
+fn roman(inscription: &str) -> usize {
+    todo!()
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+//TDD way
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_roman_iii() {
+        let roman_no = "III";
+        assert_eq!(roman(roman_no), 3);
+    }
+
+    #[test]
+    fn test_roman_vi() {
+        let roman_no = "vi";
+        assert_eq!(roman(roman_no), 6);
+    }
+
+    #[test]
+    fn test_roman_ix() {
+        let roman_no = "IX";
+        assert_eq!(roman(roman_no), 9);
+    }
+
+    #[test]
+    fn test_roman_xix() {
+        let roman_no = "XIX";
+        assert_eq!(roman(roman_no), 19);
+    }
+
+    #[test]
+    fn test_roman_mcmx() {
+        let roman_no = "MCMX";
+        assert_eq!(roman(roman_no), 1910);
+    }
+
+    #[test]
+    fn test_roman_empty() {
+        let roman_no = "";
+        assert_eq!(roman(roman_no), 0);
+    }
+
+    #[test]
+    fn test_roman_lower_case() {
+        let roman_no = "iv";
+        assert_eq!(roman(roman_no), 4);
+    }
 }
