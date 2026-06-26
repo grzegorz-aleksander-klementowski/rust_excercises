07b682ea861027db600856e883337c8495fb634d
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-06-26 23:42:00 +0200

    exercises-uni/z4/2a/test: Written loops to identify algoritm pattern of roman numbers from the exersise

--- other/rust-fundamentals-engineered-from-pl-uni/z4/a/z2-roman-num-into-arabic/src/main.rs
+++ other/rust-fundamentals-engineered-from-pl-uni/z4/a/z2-roman-num-into-arabic/src/main.rs
@@ -30,12 +30,75 @@
 
 */
 
+use std::usize;
+
 fn roman(inscription: &str) -> usize {
-    todo!()
+    // String cannot be empty
+    assert!("".is_empty());
+
+    // String only in romman uppercase letters
+    let inscription = inscription.to_uppercase();
+
+    // Separate numbers into digits for sake of calculations
+    let mut separate_rom_nums: Vec<usize> = Vec::new();
+    for c in inscription.chars() {
+        let n = match c {
+            'I' => 1,
+            'V' => 5,
+            'X' => 10,
+            'L' => 50,
+            'C' => 100,
+            'M' => 1000,
+            _ => panic!("„{c}” is not a roman number!"),
+        };
+        separate_rom_nums.push(n);
+    }
+
+    //let max_figure = separate_rom_nums.iter().enumerate().max_by(compare);
+
+    let mut res = 0usize;
+
+    // Create a vec of the possible pair roman numbers
+    let mut possible_rom_nums: Vec<(usize, usize)> = Vec::new();
+    let mut iter_sep_rom_num = separate_rom_nums.windows(2);
+    while let Some(rom_n_pair) = iter_sep_rom_num.next() {
+        //.unwrap_or_else(panic!("Something went wrong: first element empty. Looks like an empty string."));
+
+        let first = rom_n_pair.first().unwrap_or(&0);
+        let second = rom_n_pair.get(1).unwrap_or(&0);
+
+        if first < second {
+            res += second - first;
+        }
+
+        possible_rom_nums.push((*first, *second));
+
+        println!("{rom_n_pair:?}");
+    }
+
+    for (first, second) in possible_rom_nums {
+        // if a first roman num is lower than the second one from the pair – it means substraction
+        // to get the correct number (ei. „IV” (1, 5) → 5 - 1 = 4)
+        if first < second {
+            res += second - first;
+        } else {
+            res += first + second;
+        }
+
+        // if it found the substraction pattern, then miss one window
+        iter_sep_rom_num.next();
+    }
+
+    res
 }
 
 fn main() {
-    println!("Hello, world!");
+    let cases = ["xix", "iii", "mcmx"];
+
+    for i in cases {
+        println!("{i}:");
+        println!("res: {}\n", roman(i));
+    }
 }
 
 //TDD way
@@ -83,5 +146,11 @@ mod tests {
     fn test_roman_lower_case() {
         let roman_no = "iv";
         assert_eq!(roman(roman_no), 4);
+    }
+
+    #[test]
+    fn test_roman_strong_case() {
+        let roman_no = "CMXCIV";
+        assert_eq!(roman(roman_no), 994);
     }
 }
