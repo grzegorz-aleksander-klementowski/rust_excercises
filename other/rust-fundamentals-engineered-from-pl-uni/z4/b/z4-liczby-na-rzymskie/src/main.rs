43fd391502ecbb14cae099aa4c7b959a915a1ab0
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-06-29 23:57:15 +0200

    exercises-uni/z4/4b/feat: prepared tests for this exercise

--- other/rust-fundamentals-engineered-from-pl-uni/z4/b/z4-liczby-na-rzymskie/src/main.rs
+++ other/rust-fundamentals-engineered-from-pl-uni/z4/b/z4-liczby-na-rzymskie/src/main.rs
@@ -15,6 +15,49 @@
    ```
  */
 
+fn na_rzymskie(liczba: usize) -> String {
+    todo!()
+}
+
 fn main() {
     println!("Hello, world!");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basic_numbers() {
+        assert_eq!(na_rzymskie(1), "I");
+        assert_eq!(na_rzymskie(2), "II");
+        assert_eq!(na_rzymskie(3), "III");
+    }
+
+    #[test]
+    fn subtractive_notation() {
+        assert_eq!(na_rzymskie(4), "IV");
+        assert_eq!(na_rzymskie(9), "IX");
+        assert_eq!(na_rzymskie(40), "XL");
+        assert_eq!(na_rzymskie(90), "XC");
+        assert_eq!(na_rzymskie(400), "CD");
+        assert_eq!(na_rzymskie(900), "CM");
+    }
+
+    #[test]
+    fn assignment_examples() {
+        assert_eq!(na_rzymskie(19), "XIX");
+        assert_eq!(na_rzymskie(1910), "MCMX");
+    }
+
+    #[test]
+    fn mixed_numbers() {
+        assert_eq!(na_rzymskie(58), "LVIII");
+        assert_eq!(na_rzymskie(99), "XCIX");
+        assert_eq!(na_rzymskie(444), "CDXLIV");
+        assert_eq!(na_rzymskie(944), "CMXLIV");
+        assert_eq!(na_rzymskie(1994), "MCMXCIV");
+        assert_eq!(na_rzymskie(2024), "MMXXIV");
+        assert_eq!(na_rzymskie(3999), "MMMCMXCIX");
+    }
 }
