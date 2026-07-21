f5439e8625ce676a159ab1af8c8fd31a6846b1e5
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-07-21 14:27:53 +0200

    other/rust-algorithms/5a/3/chore/doc: Change name of the director that contain the screenshots of the exercises from 9 to 15 from „nowe-zadania-do-przepisania” to „screenshots-of-the-exercises”. Created the exercise project crate and add there the commented description of the exercise. Next step: transfer the exercises 5a/1 and 2 into library crates, in the aim of solving 5a3.

---
+++ other/rust-algorithms/z5/a/z3-octal-dec/src/main.rs
@@ -0,0 +1,29 @@
+/*
+[PL]
+3. Napisz funkcję o nagłówku
+
+   ```
+   fn wartosc_syst8(z: &str) -> Option<u8>
+   ```
+
+   obliczającą wartość całkowitą bez znaku zapisaną w systemie ósemkowym — pod warunkiem, że mieści się na ośmiu bitach. Jeśli nie (lub w zapisie występuje znak inny niż cyfra ósemkowa lub parametr jest pusty), to wynikiem jest `None`.
+
+   **Uwaga 1:** Funkcje te należy zbudować z funkcji zadań poprzednich.
+   **Uwaga 2:** Zrób dwie wersje tej funkcji — pierwszą bez znaku zapytania, a drugą ze znakiem zapytania.
+
+[EN]
+3. Write a function about the header
+
+   ```
+   fn syst_value8(z: &str) -> Option<u8>
+   ```
+
+   that computes an unsigned integer value written in octal - provided it fits in eight bits. If not (or the notation contains a non-octal character or the parameter is empty), the result is `None`.
+
+   **Note 1:** These functions should be built from the functions of the previous tasks.
+   **Note 2:** Make two versions of this function - the first without the question mark and the second with the question mark.
+ */
+
+fn main() {
+    println!("Hello, world!");
+}
