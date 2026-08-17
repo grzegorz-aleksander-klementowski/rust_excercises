227aaaa6676c537b3513bb2066adcc38838ca8f4
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-08-17 22:17:14 +0200

    doc(5/b/z3): add exercise content into the exercise in Polish and English.

--- other/rust-algorithms/z5/b/z3-digit-into-roman-numeral/src/lib.rs
+++ other/rust-algorithms/z5/b/z3-digit-into-roman-numeral/src/lib.rs
@@ -1,3 +1,24 @@
+/*
+[PL]
+3. Napisz funkcję
+
+   ```
+   fn wartosc_cyfry_rzymskiej(c: char) -> Result<u16, String>
+   ```
+
+   która zwraca wartość cyfry rzymskiej podanej jako znak — albo opis tekstowy błędu, jeśli znak nie jest cyfrą rzymską (jednym z: I V X L C D M).
+
+
+[EN]
+3. Write:
+
+   ```rust
+   fn wartosc_cyfry_rzymskiej(c: char) -> Result<u16, String>
+   ```
+
+   It returns the value of a Roman numeral character, or a textual error description if the character is not one of `I V X L C D M`.
+ */
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
