c6e76cb4ad72eb118a8c71f96b7438f8c11c9978
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-08-18 09:03:12 +0200

    feat(other/rust-algorithms/z5/b/z2): add finished exercise – convert a roman figure into a number (`u16`).

--- other/rust-algorithms/z5/b/z3-digit-into-roman-numeral/src/lib.rs
+++ other/rust-algorithms/z5/b/z3-digit-into-roman-numeral/src/lib.rs
@@ -13,14 +13,25 @@
 3. Write:
 
    ```rust
-   fn value_roman_figure(c: char) -> Result<u16, String>
+   fn convert_roman_fig_into_number(c: char) -> Result<u16, String>
    ```
 
    It returns the value of a Roman numeral character, or a textual error description if the character is not one of `I V X L C D M`.
  */
 
-pub fn value_roman_figure(c: char) -> Result<u16, String> {
-    todo!()
+pub fn convert_roman_fig_into_number(c: char) -> Result<u16, String> {
+    match c {
+        'I' => Ok(1),
+        'V' => Ok(5),
+        'X' => Ok(10),
+        'L' => Ok(50),
+        'C' => Ok(100),
+        'D' => Ok(500),
+        'M' => Ok(1000),
+        _ => Err(format!(
+            "`{c}` is an invalid character. Cannot be converted into a number."
+        )),
+    }
 }
 
 #[cfg(test)]
@@ -29,27 +40,27 @@ mod tests {
 
     #[test]
     fn correct_roman_numerals() {
-        assert_eq!(value_roman_figure('I'), Ok(1));
-        assert_eq!(value_roman_figure('V'), Ok(5));
-        assert_eq!(value_roman_figure('X'), Ok(10));
-        assert_eq!(value_roman_figure('L'), Ok(50));
-        assert_eq!(value_roman_figure('C'), Ok(100));
-        assert_eq!(value_roman_figure('D'), Ok(500));
-        assert_eq!(value_roman_figure('M'), Ok(1000));
+        assert_eq!(convert_roman_fig_into_number('I'), Ok(1));
+        assert_eq!(convert_roman_fig_into_number('V'), Ok(5));
+        assert_eq!(convert_roman_fig_into_number('X'), Ok(10));
+        assert_eq!(convert_roman_fig_into_number('L'), Ok(50));
+        assert_eq!(convert_roman_fig_into_number('C'), Ok(100));
+        assert_eq!(convert_roman_fig_into_number('D'), Ok(500));
+        assert_eq!(convert_roman_fig_into_number('M'), Ok(1000));
     }
 
     #[test]
     fn incorrect_character_returns_error() {
-        assert!(value_roman_figure('A').is_err());
+        assert!(convert_roman_fig_into_number('A').is_err());
     }
 
     #[test]
     fn lowercase_liter_returns_error() {
-        assert!(value_roman_figure('i').is_err());
+        assert!(convert_roman_fig_into_number('i').is_err());
     }
 
     #[test]
     fn special_character_returns_error() {
-        assert!(value_roman_figure('?').is_err());
+        assert!(convert_roman_fig_into_number('?').is_err());
     }
 }
