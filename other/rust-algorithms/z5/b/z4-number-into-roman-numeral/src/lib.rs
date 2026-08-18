7de3d7d4bc77fd00d512f6da5cd032b714bd68e4
Grzegorz Aleksander Klementowski <grzegorz.aleksander@klementowski.pl>
2026-08-18 10:44:44 +0200

    doc(other/rust-algorithms/z5/b/z4): add content of the exercise.

--- other/rust-algorithms/z5/b/z4-number-into-roman-numeral/src/lib.rs
+++ other/rust-algorithms/z5/b/z4-number-into-roman-numeral/src/lib.rs
@@ -1,3 +1,29 @@
+/*
+[PL]
+4. Napisz funkcję o nagłówku
+
+   ```
+   fn rzymskie(napis: &str) -> Result<u128, String>
+   ```
+
+   która dla napisu reprezentującego liczbę w zapisie rzymskim (nie zakładamy jego poprawności; ponadto pusty ciąg także jest niepoprawny) zwraca liczbę reprezentowaną przez ów napis lub napisowy opis błędu.
+
+   Błędy mogą być trojakie — niewłaściwa cyfra (o tym powiadomi nas poprzednia funkcja pomocnicza); pusty napis; niewłaściwa kolejność cyfr.
+
+   **Uwaga:** użyj funkcji z poprzedniego zadania i operatora `?`.
+
+[EN]
+4. Write:
+
+   ```rust
+   fn rzymskie(napis: &str) -> Result<u128, String>
+   ```
+
+   It receives a possibly invalid Roman-numeral string; an empty string is invalid too. Return the represented number or an error description. Errors include an invalid digit, an empty string, and invalid digit order.
+
+   **Note:** Use the preceding function and the `?` operator.
+ */
+
 pub fn add(left: u64, right: u64) -> u64 {
     left + right
 }
